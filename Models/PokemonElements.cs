0a180ba3990705d0765604815bb1c3fe15348287
Carter Small <csmall@student.neumont.edu>
2023-02-14 17:46:07 -0700

    updated stuff

--- Models/PokemonElements.cs
+++ Models/PokemonElements.cs
@@ -5,23 +5,47 @@ using System.Text.Json.Serialization;
 namespace Poke_Adventures.Models
 {
     public class PokemonElements
-    {
-        
-            [JsonPropertyName("is_hidden")]
-            public bool IsHidden { get; set; }
+    {       
 
-            [JsonPropertyName("name")]
-            public string Name { get; set; }
+        [JsonPropertyName("abilities")]
+         public List<AbilityProp>? Abilities { get; set; }
 
-            [JsonPropertyName("url")]
-            public Uri Url { get; set; }
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
 
+        [JsonPropertyName("url")]
+        public Uri? Url { get; set; }
 
-            public static PokemonElements LoadPokemon(int num)
-            {  
-                string json = new WebClient().DownloadString($"https://pokeapi.co/api/v2/pokemon/{num}");
-                PokemonElements pokemon = JsonConvert.DeserializeObject<PokemonElements>(json);
-                return pokemon;
-            }   
+        public static PokemonElements LoadPokemon(int num)
+        {
+            string json = new WebClient().DownloadString($"https://pokeapi.co/api/v2/pokemon/{num}");
+            PokemonElements pokemon = JsonConvert.DeserializeObject<PokemonElements>(json);
+            return pokemon;
+        }
+
+        public static PokemonElements LoadAbility(int num)
+        {
+            string json = new WebClient().DownloadString($"https://pokeapi.co/api/v2/ability/{num}");
+            PokemonElements pokemon = JsonConvert.DeserializeObject<PokemonElements>(json);
+            return pokemon;
+        }
     }
+    public class AbilityProp
+    {
+        [JsonPropertyName("ability")]
+        public Common? AbilityAbility { get; set; }
+
+        [JsonPropertyName("is_hidden")]
+        public bool IsHidden { get; set; }
+    }
+
+    public class Common
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("url")]
+        public Uri? Url { get; set; }
+    }
+
 }
