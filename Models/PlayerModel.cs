da56f797684e492625b8dd9d73d85638c20576b7
leo5d1 <80261451+leo5d1@users.noreply.github.com>
2023-02-27 16:10:05 -0700

    Trainer Ai

--- Models/PlayerModel.cs
+++ Models/PlayerModel.cs
@@ -2,6 +2,38 @@
 {
     public class PlayerModel
     {
-        public static List<string> PlayerTeam = new List<string> { "voltorb", "miltank", "charmander" };
+        public static List<CharacterPokemon> PlayerTeam = new List<CharacterPokemon> { };
+
+        public static int avglvl = GetAverageLevel();
+
+        public static void AddPokemon(string name, int lvl)
+        {
+            CharacterPokemon mon = new CharacterPokemon(name, lvl);
+
+            PlayerTeam.Add(mon);
+        }
+
+        public static int GetAverageLevel()
+        {
+            int averagelvl = 0;
+            for (int i = 1; i < PlayerTeam.Count; i++)
+            {
+                averagelvl += PlayerTeam[i - 1].Level;
+            }
+            
+            return averagelvl / PlayerTeam.Count;
+        }
+    }
+
+    public class CharacterPokemon
+    {
+        public CharacterPokemon(string name, int lvl)
+        {
+            Name = name;
+            Level = lvl;
+        }
+
+        public string Name;
+        public int Level;
     }
 }
--- Models/TrainerModel.cs
+++ Models/TrainerModel.cs
@@ -1,39 +1,61 @@
 ﻿
-
 namespace Poke_Adventures.Models
 {
     public class TrainerModel
     {          
         // Trainer Team
-        public static List<string> TrainerTeam = new List<string> { };
-
+        public static List<CharacterPokemon> TrainerTeam = new List<CharacterPokemon> { };
 
         // randomize team based on difficulty
-        public void RandomizeTeam(string difficulty)
+        public static void RandomizeTeam(string difficulty)
         {
+            int min = 0;
+            int max = 0;
             switch(difficulty)
             {
                 case "Easy":
-                    // min = plvlavg - 3
-                    // max = plvlavg + 2
+                    min = PlayerModel.avglvl - 3;
+                    max = PlayerModel.avglvl + 2;
                     // pool = player pool
+                    RandomPokemon(min, max);
                     break;
 
                 case "Medium":
-                    // min = plvlavg
-                    // max = plvlavg + 5
+                    min = PlayerModel.avglvl;
+                    max = PlayerModel.avglvl + 5;
                     // pool = player pool
+                    RandomPokemon(min, max);
                     break;
 
                 case "Hard":
-                    // min = plvlavg + 1
-                    // max = plvlavg + 8
+                    min = PlayerModel.avglvl + 1;
+                    max = PlayerModel.avglvl + 8;
                     // pool = player pool + 1
+                    RandomPokemon(min, max);
                     break;
             }
         }
 
+        public static void RandomPokemon(int lvlMin, int lvlMax)
+        {
+            TrainerTeam.Clear();
 
-        // 
+            Random rand = new Random();
+            var allPokemon = Pokemon.LoadAllPokemon();
+
+            // gets three random pokemon
+            for (int i = 0; i < 3; i++)
+            {
+                int level = rand.Next(lvlMin, lvlMax);
+                var pokemonIndex = rand.Next(allPokemon.Results.Count);
+                var pokemonName = PokemonElements.LoadPokemon(pokemonIndex);
+
+
+                CharacterPokemon pokemon = new CharacterPokemon(pokemonName.Name, level);
+
+                TrainerTeam.Add(pokemon);
+            }
+
+        }
     }
 }
