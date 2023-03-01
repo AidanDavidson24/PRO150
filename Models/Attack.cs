d9e0afbded8da60969f7ae329b51c8eec63fde0e
leo5d1 <80261451+leo5d1@users.noreply.github.com>
2023-02-28 18:22:47 -0700

    Merge Done

--- Models/Attack.cs
+++ Models/Attack.cs
@@ -8,9 +8,9 @@
         {
             int? currentHP = curHP;
 
-            int? damage = PokemonModel.AttackDamage(MoveElements.LoadMove(PokemonElements.LoadPokemon(PlayerModel.Team()[3]).Moves[0].Move).ID, PlayerModel.Team()[0], PlayerModel.Team()[1]);
+            int? damage = PokemonModel.AttackDamage(MoveElements.LoadMove(PokemonElements.LoadPokemon(PlayerModel.PlayerTeam[].Name).Moves[0].Move).ID, PlayerModel.PlayerTeam[0].Name, PlayerModel.PlayerTeam[1].Name);
 
-            System.Diagnostics.Debug.WriteLine(PokemonModel.AttackDamage(5, PlayerModel.Team()[0], PlayerModel.Team()[1]) + " Move");
+            System.Diagnostics.Debug.WriteLine(PokemonModel.AttackDamage(5, PlayerModel.PlayerTeam[0].Name, PlayerModel.PlayerTeam[1].Name) + " Move");
 
             System.Diagnostics.Debug.WriteLine(currentHP + " Enemy HP");
 
@@ -25,18 +25,18 @@
         {
             List<int> Moves = new List<int> 
             {
-                MoveElements.LoadMove(PokemonElements.LoadPokemon(PlayerModel.Team()[1]).Moves[2].Move).ID,
-                MoveElements.LoadMove(PokemonElements.LoadPokemon(PlayerModel.Team()[1]).Moves[4].Move).ID,
-                MoveElements.LoadMove(PokemonElements.LoadPokemon(PlayerModel.Team()[1]).Moves[5].Move).ID,
-                MoveElements.LoadMove(PokemonElements.LoadPokemon(PlayerModel.Team()[1]).Moves[6].Move).ID
+                MoveElements.LoadMove(PokemonElements.LoadPokemon(PlayerModel.PlayerTeam[1].Name).Moves[2].Move).ID,
+                MoveElements.LoadMove(PokemonElements.LoadPokemon(PlayerModel.PlayerTeam[1].Name).Moves[4].Move).ID,
+                MoveElements.LoadMove(PokemonElements.LoadPokemon(PlayerModel.PlayerTeam[1].Name).Moves[5].Move).ID,
+                MoveElements.LoadMove(PokemonElements.LoadPokemon(PlayerModel.PlayerTeam[1].Name).Moves[6].Move).ID
             };
             Random random = new Random();
 
             int? currentHP = curHP;
 
-            int? damage = PokemonModel.AttackDamage(Moves[random.Next(Moves.Count())], PlayerModel.Team()[1], PlayerModel.Team()[4]);
+            int? damage = PokemonModel.AttackDamage(Moves[random.Next(Moves.Count())], PlayerModel.PlayerTeam[1].Name, PlayerModel.PlayerTeam[4].Name);
 
-            System.Diagnostics.Debug.WriteLine(PokemonModel.AttackDamage(5, PlayerModel.Team()[1], PlayerModel.Team()[4]) + " Move");
+            System.Diagnostics.Debug.WriteLine(PokemonModel.AttackDamage(5, PlayerModel.PlayerTeam[1].Name, PlayerModel.PlayerTeam[4].Name) + " Move");
             System.Diagnostics.Debug.WriteLine(Moves[random.Next(Moves.Count())] + " Moves");
 
             System.Diagnostics.Debug.WriteLine(currentHP + " Char HP");
