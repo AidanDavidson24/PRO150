e2486448dfa93f2020f6f49d47d01bd69445f147
Aidan Mark-Davidson <amarkdavidson@student.neumont.edu>
2023-03-07 07:53:36 -0700

    more login stuff

---
+++ Data/PokeAdventureDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+using Poke_Adventures.Models;
+
+namespace Poke_Adventures.Data
+{
+		public class PokeAdventureDbContext : IdentityDbContext
+		{
+			public PokeAdventureDbContext(DbContextOptions<PokeAdventureDbContext> options)
+				: base(options)
+			{
+			}
+
+			public DbSet<UserName> UserNames { get; set; }
+			public DbSet<Password> passwords { get; set; }
+			public DbSet<Comment> Comments { get; set; }
+		}
+}
